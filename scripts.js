61a247a0a36fdba89a2e3533ef2d99d2f150ed1a
Szollosigy11 <33136248+Szollosigy11@users.noreply.github.com>
2020-04-10 17:32:07 +0200

    coloring buttons

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
-$('#yellow').click(() => {
-    .content{
-        backgroundcolor: yellow;
-    }
+$("#yellow").click(() => {
+    $(".content").css("background","yellow")
 })
+
+$("#green").click(() => {
+    $(".content").css("background","green")
+})
+
+$("#blue").click(() => {
+    $(".content").css("background","blue")
+})
+
+$("#gray").click(() => {
+    $(".content").css("background","lightgray")
+})
+
